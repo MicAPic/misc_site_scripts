dc74f25a45d83df977f0eb89a32dddd0d88aef3b
Michael Pavlov <73939030+MicAPic@users.noreply.github.com>
2021-04-16 23:08:19 +0300

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,111 +1,16 @@
-// This is awful, don't look at this code
-
-function hide1() {
-  let object = document.getElementById("c1"),
-      object_tab = document.getElementById("t1"),
-      x = document.getElementById("c2"),
-      x_tab = document.getElementById("t2"),
-      y = document.getElementById("c3"),
-      y_tab = document.getElementById("t3"),
-      z = document.getElementById("c4"),
-      z_tab = document.getElementById("t4");
+function hide(id) {
+  let object = document.getElementById("c" + id),
+      object_tab = document.getElementById(id),
+      containers = document.querySelectorAll('[id^="ctab"]'),
+      tabs = document.querySelectorAll('[id^="tab"]');
   if (object.style.display === "none") {
-    x.style.display = "none";
-    x_tab.style.color = "a0a0a0";
-    x_tab.style.borderBottom = "4px solid transparent";
-    y.style.display = "none";
-    y_tab.style.color = "a0a0a0";
-    y_tab.style.borderBottom = "4px solid transparent";
-    z.style.display = "none";
-    z_tab.style.color = "a0a0a0";
-    z_tab.style.borderBottom = "4px solid transparent";
-    object.setAttribute("style", "display: grid;\n" +
-        "\tgap: 45px 183px;\n" +
-        "\tgrid-template-columns: 405px 405px;\n" +
-        "\tjustify-content: center;\n" +
-        "\tmargin-top: -835px;");
-    object_tab.style.color = "white";
-    object_tab.style.borderBottom = "4px solid white";
-  }
-}
-
-function hide2() {
-  let object = document.getElementById("c2"),
-      object_tab = document.getElementById("t2"),
-      x = document.getElementById("c1"),
-      x_tab = document.getElementById("t1"),
-      y = document.getElementById("c3"),
-      y_tab = document.getElementById("t3"),
-      z = document.getElementById("c4"),
-      z_tab = document.getElementById("t4");
- if (object.style.display === "none") {
-    x.style.display = "none";
-    x_tab.style.color = "a0a0a0";
-    x_tab.style.borderBottom = "4px solid transparent";
-    y.style.display = "none";
-    y_tab.style.color = "a0a0a0";
-    y_tab.style.borderBottom = "4px solid transparent";
-    z.style.display = "none";
-    z_tab.style.color = "a0a0a0";
-    z_tab.style.borderBottom = "4px solid transparent";
-    object.setAttribute("style", "display: grid;\n" +
-        "\tgap: 45px 183px;\n" +
-        "\tgrid-template-columns: 405px 405px;\n" +
-        "\tjustify-content: center;\n" +
-        "\tmargin-top: -835px;");
-    object_tab.style.color = "white";
-    object_tab.style.borderBottom = "4px solid white";
-  }
-}
-
-function hide3() {
-  let object = document.getElementById("c3"),
-      object_tab = document.getElementById("t3"),
-      x = document.getElementById("c2"),
-      x_tab = document.getElementById("t2"),
-      y = document.getElementById("c1"),
-      y_tab = document.getElementById("t1"),
-      z = document.getElementById("c4"),
-      z_tab = document.getElementById("t4");
-  if (object.style.display === "none") {
-    x.style.display = "none";
-    x_tab.style.color = "a0a0a0";
-    x_tab.style.borderBottom = "4px solid transparent";
-    y.style.display = "none";
-    y_tab.style.color = "a0a0a0";
-    y_tab.style.borderBottom = "4px solid transparent";
-    z.style.display = "none";
-    z_tab.style.color = "a0a0a0";
-    z_tab.style.borderBottom = "4px solid transparent";
-    object.setAttribute("style", "display: grid;\n" +
-        "\tgap: 45px 183px;\n" +
-        "\tgrid-template-columns: 405px 405px;\n" +
-        "\tjustify-content: center;\n" +
-        "\tmargin-top: -835px;");
-    object_tab.style.color = "white";
-    object_tab.style.borderBottom = "4px solid white";
-  }
-}
-
-function hide4() {
-  let object = document.getElementById("c4"),
-      object_tab = document.getElementById("t4"),
-      x = document.getElementById("c2"),
-      x_tab = document.getElementById("t2"),
-      y = document.getElementById("c3"),
-      y_tab = document.getElementById("t3"),
-      z = document.getElementById("c1"),
-      z_tab = document.getElementById("t1");
-  if (object.style.display === "none") {
-    x.style.display = "none";
-    x_tab.style.color = "a0a0a0";
-    x_tab.style.borderBottom = "4px solid transparent";
-    y.style.display = "none";
-    y_tab.style.color = "a0a0a0";
-    y_tab.style.borderBottom = "4px solid transparent";
-    z.style.display = "none";
-    z_tab.style.color = "a0a0a0";
-    z_tab.style.borderBottom = "4px solid transparent";
+   for(var container = 0; container < containers.length; ++container) {
+           containers[container].style.display = "none";
+        }
+    for(var tab = 0; tab < tabs.length; ++tab) {
+           tabs[tab].style.color = "a0a0a0";
+           tabs[tab].style.borderBottom = "4px solid transparent";
+        }
     object.setAttribute("style", "display: grid;\n" +
         "\tgap: 45px 183px;\n" +
         "\tgrid-template-columns: 405px 405px;\n" +
